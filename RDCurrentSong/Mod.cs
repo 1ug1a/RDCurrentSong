c1de64a6efe3c79f2f819cbad414848ea795f440
1ug1a <elpy.perez@gmail.com>
2020-08-24 16:50:58 -0700

    Add project files.

---
+++ RDCurrentSong/Mod.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace RDCurrentSong
+{
+    [BepInPlugin("com.rhythmdr.currentsong", "Rhythm Doctor Current Song Plugin", "1.2")]
+    [BepInProcess("Rhythm Doctor Preorder.exe")]
+    public class Mod : BaseUnityPlugin
+    {
+        private ConfigEntry<string> configName;
+        private ConfigEntry<string> configSplitName;
+        private ConfigEntry<string> configFolder;
+        private ConfigEntry<int> configSpacing;
+        private ConfigEntry<int> configMinLength;
+        private ConfigEntry<bool> configSplit;
+
+        private int spacing, minLength;
+        private string oldLevel, newLevel, folder, folder2;
+        private string[] splitLevel;
+
+        void Awake()
+        {
+            configFolder = Config.Bind("General",
+                                       "FolderPath",
+                                       "",
+                                       "The system path where the .txt file is located. " +
+                                       "Leave blank to use game folder.");
+
+            configName = Config.Bind("General",
+                           "FileName",
+                           "level",
+                           "The name of the .txt file (without .txt).");
+
+            configMinLength = Config.Bind("General",
+                                          "MinLength",
+                                          15,
+                                          "The minimum length of the song title. Adds extra spaces if the title is short.");
+
+            configSpacing = Config.Bind("General",
+                                        "SpacingAmt",
+                                        5,
+                                        "The number of spaces added after the song title, no matter the length.");
+
+            configSplit = Config.Bind("General",
+                                      "SplitTitle",
+                                      false,
+                                      "Whether or not to split song name and artist into separate files.");
+
+            configSplitName = Config.Bind("General",
+                                          "SplitName",
+                                          "artist",
+                                          "The name of the song artist .txt file (without .txt)");
+
+            if (configFolder.Value == "")
+            {
+                folder = Path.Combine(Directory.GetParent(Application.dataPath).FullName, configName.Value + ".txt");
+                folder2 = Path.Combine(Directory.GetParent(Application.dataPath).FullName, configSplitName.Value + ".txt");
+            }
+            else
+            {
+                folder = Path.Combine(configFolder.Value, configName.Value + ".txt");
+                folder2 = Path.Combine(configFolder.Value, configSplitName.Value + ".txt");
+            }
+
+            minLength = configMinLength.Value;
+            spacing = configSpacing.Value;
+
+            newLevel = "";
+
+            using (StreamWriter outputFile = new StreamWriter(folder))
+            {
+                outputFile.WriteLine(newLevel.PadRight(minLength) + "".PadRight(spacing));
+            }
+        }
+        void Update()
+        {
+            newLevel = DiscordController.instance.cachedLevelName;
+            if (oldLevel != newLevel)
+            {
+                if (configSplit.Value == false)
+                {
+                    using (StreamWriter outputFile = new StreamWriter(folder))
+                    {
+                        outputFile.WriteLine(newLevel.PadRight(minLength) + "".PadRight(spacing));
+                    }
+                }
+                else
+                {
+                    splitLevel = newLevel.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+                    using (StreamWriter outputFile = new StreamWriter(folder))
+                    {
+                        outputFile.WriteLine(splitLevel[1]);
+                    }
+                    using (StreamWriter outputFile = new StreamWriter(folder2))
+                    {
+                        outputFile.WriteLine(splitLevel[0]);
+                    }
+                }
+            }
+            oldLevel = newLevel;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Rhythm Doctor Current Song Plugin
+*A [BepinEx](https://github.com/BepInEx/BepInEx/releases) plugin for Rhythm Doctor.*
+
+Takes the current level's song and artist name and outputs it into a `.txt` file, for use in OBS or other streaming software.
+
+How to install:
+1. Download the latest release of BepinEx_x86 and `RDCurrentSong.dll`.
+2. Extract it to the folder containing the game `.exe`. 
+3. Run the game. BepinEx should create all of its necessary folders.
+4. Put `RDCurrentSong.dll` into the `BepinEx/plugins` folder.
+5. Run the game again. By default, it'll output the `.txt` file to the folder containing the game `.exe`. You can change this in the config file.
+
+Required libraries to compile the `.dll` yourself (not included in `Libs`):
+* BepinEx.dll (from `BepinEx/core`)
+* UnityEngine.dll (from `Rhythm Doctor Preorder_data/Managed`)
+* UnityEngine.CoreModule.dll (from `Rhythm Doctor Preorder_data/Managed`)
+* Assembly-CSharp.dll (from `Rhythm Doctor Preorder_data/Managed`)
